62c5df8f484b47ead2eda28971084aa2914fd77c
Chunpo Wang <artoowang@gmail.com>
2021-11-26 09:44:30 +0800

    Fixed setup.py again.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ with open('README.md', 'r') as fh:
 setuptools.setup(
     name='cython_colab',
     version='0.1',
-    scripts=['cython_colab.py'] ,
     author='Chunpo Wang',
     author_email='artoowang@gmail.com',
     description='Cython Colab Utilities',
@@ -15,6 +14,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/artoowang/ray-tracer-colab',
     license='GNU General Public License v3 (GPLv3)',
+    # This is needed for single module without a package (directory).
+    # See https://docs.python.org/3/distutils/examples.html#pure-python-distribution-by-module
+    py_modules=['cython_colab'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
